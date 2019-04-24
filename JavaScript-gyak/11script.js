b670bc6b77c4bebe85b2c70266814b7b078864b9
Palko Adam <palkoadam93@gmail.com>
2019-04-24 12:09:44 +0200

    régiek hozzáadása

---
+++ JavaScript-gyak/11script.js
@@ -0,0 +1,3 @@
+function myFunction() {
+   document.getElementById("demo").innerHTML = "A külső '11script.js' nevű fájlban található függvény helyezte ezt a szöveget a bekezdésbe.";
+}
---
+++ Tanulási-stílusok/Tanulási stílusok   lélektani típusok_files/dynamic_css.php
@@ -0,0 +1,5 @@
+/* random banner */
+
+#header {
+  background: #000033 url('/sites/default/files/images/banner/nyar/6.jpg');
+}
